9f12cef4a3df27810c21620c207e3735346dc13a
wahyuhidayattz <wahyuhidayatsmk2@gmail.com>
2022-08-25 12:46:50 +0700

    Update

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,16 @@
+<div class="flex flex-row items-center justify-between bg-white shadow-lg container-x">
+    <h1 class="flex flex-row items-center gap-3 text-lg font-bold text-violet-600">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
+        </svg>
+        PHP Pedia
+    </h1>
+    <div class="flex flex-row items-center gap-3 text-sm text-gray-500">
+        <a href=""
+            class="flex flex-row items-center px-4 py-2 border border-gray-200 rounded-lg hover:bg-gray-200">Login</a>
+        <a href=""
+            class="flex flex-row items-center px-4 py-2 text-white rounded-lg bg-violet-500 hover:bg-violet-600">Logout</a>
+    </div>
+</div>
